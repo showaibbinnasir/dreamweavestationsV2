77fd647218afd221ed448b68bdeac5018cf1a768
showaibbinnasir <showaibnishad@gmail.com>
2025-02-18 10:48:45 -0600

    added again

--- app/components/CustomerReviews/ReviewSlider.js
+++ app/components/CustomerReviews/ReviewSlider.js
@@ -9,43 +9,12 @@ import {
     CarouselPrevButton,
     CarouselSlides,
 } from 'keep-react'
+import Image from 'next/image'
 
 
 export default function ReviewSlider({ data }) {
     return (
         <div className='mx-5 my-5'>
-            {/* <Carousel options={{ loop: true }} plugins={[autoPlay()]}>
-                <CarouselSlides>
-                    {data.map((review, i) => (
-                        <CarouselItem key={i}>
-                            <div className='flex justify-center'>
-                                <div className='bg-[#18181C] p-10 h-[470px] w-[300px] lg:w-[750px] flex items-center rounded-md'>
-                                    <div>
-                                        {
-                                            review?.des.length > 200 ? 
-                                            <h1 className='my-5'> {review?.des.substring(0, 300)}.... </h1> : 
-                                            <h1 className='my-5'>{review?.des}</h1>
-                                        }
-                                        <hr />
-                                        <div className='flex items-center gap-2 mt-5'>
-                                            <img className='w-[40px] h-[40px] rounded-full bg-cover' src={review.image} alt="" />
-                                            <h1 className='text-md font-semibold'>{review.name}</h1>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </div>
-                        </CarouselItem>
-                    ))}
-                </CarouselSlides>
-                <CarouselControl>
-                    <CarouselButtons>
-                        <CarouselPrevButton />
-                        <CarouselNextButton />
-                    </CarouselButtons>
-                    <CarouselIndicators />
-                </CarouselControl>
-            </Carousel> */}
             <Carousel options={{ slidesToScroll: 1 }}>
                 <CarouselSlides className="flex">
                     {data.map((review, i) => (
@@ -60,7 +29,7 @@ export default function ReviewSlider({ data }) {
                                         }
                                         <hr />
                                         <div className='flex items-center gap-2 mt-5'>
-                                            <img className='w-[40px] h-[40px] rounded-full bg-cover' src={review.image} alt="" />
+                                            <Image className='w-[40px] h-[40px] rounded-full object-cover' width={50} height={50} src={review.image} alt={`${review.name}`} unoptimized/>
                                             <h1 className='text-md font-semibold'>{review.name}</h1>
                                         </div>
 
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,13 @@ const config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  theme: {
+    extend: {
+      animation: {
+        'slow-spin': 'spin 12s linear infinite', // Adjust duration (8s here)
+      },
+    },
+  },
   plugins: [],
 };
 
