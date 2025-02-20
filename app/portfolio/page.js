c31d7440bf83186e604fc66cf6e180c408249021
showaibbinnasir <showaibnishad@gmail.com>
2025-02-19 23:58:23 -0600

    added portfolio

--- app/portfolio/page.js
+++ app/portfolio/page.js
@@ -1,7 +1,33 @@
 import React from 'react'
+import OurWorks from '../components/OurWorks/OurWorks'
+import CustomerReviews from '../components/CustomerReviews/CustomerReviews'
+
+export const metadata = {
+  title: "Dreamweave Stations | Portfolio",
+  description: "Look at our previous works",
+  openGraph: {
+  title: "Dreamweave Stations | Portfolio",
+    description: "Look at our previous works",
+    url: "https://dreamweavev2.netlify.app/portfolio",
+    type: "website",
+    images: [
+      {
+        url: "https://nomanandrohangcm.com/work1.jpg", // Thumbnail URL
+        width: 1200,
+        height: 630,
+        alt: "Thumbnail Image",
+      },
+    ],
+  },
+};
 
 export default function Portfolio() {
   return (
-    <div>This is portfolio page</div> 
+    <div>
+      <div>
+        <OurWorks></OurWorks>
+      </div>
+      
+    </div> 
   )
 }
